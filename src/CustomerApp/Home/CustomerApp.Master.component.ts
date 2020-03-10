475085556ccaf396411ed6b2cf560ee4461da349
Sujan.Lamichhane <sujann42@gmail.com>
2020-03-10 16:31:37 -0400

    more updates

--- src/CustomerApp/Home/CustomerApp.Master.component.ts
+++ src/CustomerApp/Home/CustomerApp.Master.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './CustomerApp.MasterPageView.html'
+  templateUrl: './CustomerApp.MasterPageView.html',
+  styleUrls: ['./CustomerApp.Master.scss']
 })
 
 
